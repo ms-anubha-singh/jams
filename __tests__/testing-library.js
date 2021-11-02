2a618f8655a12503398ada9a785ab89c83f6230f
Anubha Singh <45950125+anubha22@users.noreply.github.com>
2021-11-02 11:20:56 +0000

    Adding unit test for Jam API

--- __tests__/testing-library.js
+++ __tests__/testing-library.js
@@ -31,3 +31,79 @@ describe('App', () => {
     expect(heading).toBeInTheDocument();
   });
 });
+
+const fetchAllData = () => ({
+  adminId: 'auth0|614c41b3de45d300692bc589',
+  isOpen: true,
+  createdAt: '1 October 2021 at 19:19:43 PM UTC+1',
+  description: 'How well can you folk',
+  urlPath: 's6p1qv76bxg',
+  name: 'Folk Jam',
+  key: 'hZLVrK2lhig19cbLuMBx',
+});
+
+const fetchDataNoAdminToken = () => ({
+  isOpen: true,
+  createdAt: '1 October 2021 at 19:19:43 PM UTC+1',
+  description: 'How well can you folk',
+  urlPath: 's6p1qv76bxg',
+  name: 'Folk Jam',
+  key: 'hZLVrK2lhig19cbLuMBx',
+});
+
+const fetchDataInvalidAdmin = () => ({
+  adminId: 'blahblahblah',
+  isOpen: true,
+  createdAt: '1 October 2021 at 19:19:43 PM UTC+1',
+  description: 'How well can you folk',
+  urlPath: 's6p1qv76bxg',
+  name: 'Folk Jam',
+  key: 'hZLVrK2lhig19cbLuMBx',
+});
+
+// NEWly added
+describe('Testing api for getting a jam:', () => {
+  it('with admin token', async () => {
+    const fakeData = {
+      adminId: 'auth0|614c41b3de45d300692bc589',
+      isOpen: true,
+      createdAt: '1 October 2021 at 19:19:43 PM UTC+1',
+      description: 'How well can you folk',
+      urlPath: 's6p1qv76bxg',
+      name: 'Folk Jam',
+      key: 'hZLVrK2lhig19cbLuMBx',
+    };
+
+    const data = await fetchAllData();
+    expect(data).toStrictEqual(fakeData);
+  });
+
+  it('without admin token', async () => {
+    const fakeData = {
+      isOpen: true,
+      createdAt: '1 October 2021 at 19:19:43 PM UTC+1',
+      description: 'How well can you folk',
+      urlPath: 's6p1qv76bxg',
+      name: 'Folk Jam',
+      key: 'hZLVrK2lhig19cbLuMBx',
+    };
+
+    const data = await fetchDataNoAdminToken();
+    expect(data).toStrictEqual(fakeData);
+  });
+
+  it('invalid admin token', async () => {
+    const fetchData = {
+      adminId: 'blah_blah_blah_invalid_admin_token',
+      isOpen: true,
+      createdAt: '1 October 2021 at 19:19:43 PM UTC+1',
+      description: 'How well can you folk',
+      urlPath: 's6p1qv76bxg',
+      name: 'Folk Jam',
+      key: 'hZLVrK2lhig19cbLuMBx',
+    };
+
+    const data = await fetchDataInvalidAdmin();
+    expect(data).toStrictEqual(fetchData);
+  });
+});
